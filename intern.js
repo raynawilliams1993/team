a8deafc7feb836bef4a720a322403b5bbaa314cf
rayna williams <Raynawilliams123@gmail.com>
2020-01-20 12:13:12 -0700

    added js files for each class, as well as an html renderer page, pseudocoded also

---
+++ intern.js
@@ -0,0 +1,5 @@
+// The Intern class `extends` from Employee, and should have these additional properties/behaviors:
+
+// school
+// getSchool()
+// getRole() (Overridden to return 'Intern')
---
+++ manager.js
@@ -0,0 +1,4 @@
+// The Manager class `extends` from Employee, and should have these additional properties/behaviors:
+
+// officeNumber
+// getRole() (Overridden to return 'Manager')
